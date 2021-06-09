dd628482bcb793bfee89eb21b94fbee85aab20c5
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-06-09 17:12:38 -0400

    fixed auto-fill issue for company member contact

--- src/main/www/src/Utils/formFunctionHelpers.js
+++ src/main/www/src/Utils/formFunctionHelpers.js
@@ -120,7 +120,7 @@ export function matchContactFields(existingContactData) {
   );
 
   return {
-    company: {
+    member: {
       id: existingCompanyContact?.id || '',
       firstName: existingCompanyContact?.first_name || '',
       lastName: existingCompanyContact?.last_name || '',
@@ -315,7 +315,7 @@ export async function executeSendDataByStep(step, formData, formId, userId) {
         formId,
         end_point.contacts,
         matchContactFieldsToBackend(
-          formData.representative.company,
+          formData.representative.member,
           contact_type.COMPANY,
           formId
         )
