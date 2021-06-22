f0d96442f40f0c47e9376b44c310da171a5f7a27
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-06-22 13:40:55 -0400

    Added internal error 50x component

--- src/main/www/src/components/Pages/ErrorPages/InternalError50x.js
+++ src/main/www/src/components/Pages/ErrorPages/InternalError50x.js
@@ -1,3 +1,9 @@
 export default function InternalError50x() {
-  return <div></div>;
+  return (
+    <>
+      <h1>An unexpected error occurred</h1>
+      <p>Sorry, the page you are looking for is currently unavailable.</p>
+      <p>Please try again later.</p>
+    </>
+  );
 }
