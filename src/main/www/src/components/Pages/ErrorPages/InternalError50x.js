afc99d4b56070dd53bac43e3daccc8a734eda042
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-06-22 13:39:38 -0400

    Added not found 404 component

---
+++ src/main/www/src/components/Pages/ErrorPages/InternalError50x.js
@@ -0,0 +1,3 @@
+export default function InternalError50x() {
+  return <div></div>;
+}
---
+++ src/main/www/src/components/Pages/ErrorPages/NotFound404.js
@@ -0,0 +1,33 @@
+export default function NotFound404() {
+  return (
+    <>
+      <h1>404 File not found</h1>
+      <p>We're sorry, the page you requested cannot be found. Below are some links that may be useful.</p>
+      <ul>
+        <li>
+          <a href="https://eclipse.org/">Eclipse home</a>
+        </li>
+        <li>
+          <a href="https://eclipse.org/projects/searchpage.php">Search</a> our site
+        </li>
+        <li>
+          Website
+          <a href="https://eclipse.org/legal/privacy.php">privacy policy</a> and
+          <a href="https://eclipse.org/legal/termsofuse.php">terms of use</a>
+        </li>
+        <li>
+          <a href="https://eclipse.org/legal">Legal documents</a>
+        </li>
+        <li>
+          <a href="https://eclipse.org/artwork/">Logos</a> and artwork
+        </li>
+        <li>
+          <a href="https://wiki.eclipse.org/index.php/Webmaster_FAQ">webmaster FAQ</a>
+        </li>
+        <li>
+          <a href="mailto:webmaster@eclipse.org">Email the webmaster</a>
+        </li>
+      </ul>
+    </>
+  );
+}
