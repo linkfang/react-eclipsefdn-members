8328df9f1750095b49203df41e3521cdcf133b0d
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-07-09 17:44:42 -0400

    finished the basic faq collapse list

--- src/main/www/src/components/PagesPortal/Dashboard/Dashboard.tsx
+++ src/main/www/src/components/PagesPortal/Dashboard/Dashboard.tsx
@@ -1,11 +1,52 @@
 import { useState } from 'react';
 import { List, ListItem, ListItemText, Collapse } from '@material-ui/core';
 
+const faqItems = [
+  {
+    question: 'Question-1',
+    answer:
+      "Lorem Ipsum 1 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  },
+  {
+    question: 'Question-2',
+    answer:
+      "Lorem Ipsum 2 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  },
+  {
+    question: 'Question-3',
+    answer:
+      "Lorem Ipsum 3 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  },
+  {
+    question: 'Question-4',
+    answer:
+      "Lorem Ipsum 4 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  },
+];
+
 export default function Dashboard() {
-  const [open, setOpen] = useState(true);
-  const handleClick = () => {
-    setOpen(!open);
+  const [shouldCollapse, setShouldCollapse] = useState<Array<boolean>>([]);
+
+  const handleClick = (index: number) => {
+    const newShouldCollapse = [...shouldCollapse];
+    newShouldCollapse[index] = !newShouldCollapse[index];
+    setShouldCollapse(newShouldCollapse);
   };
+
+  const renderFAQs = faqItems.map((item, index) => (
+    <div key={index}>
+      <ListItem>
+        <ListItemText primary={item.question} onClick={() => handleClick(index)} />
+      </ListItem>
+      <Collapse in={shouldCollapse[index]} timeout="auto" unmountOnExit>
+        <List disablePadding>
+          <ListItem>
+            <ListItemText primary={item.answer} />
+          </ListItem>
+        </List>
+      </Collapse>
+    </div>
+  ));
 
   return (
     <div>
@@ -15,18 +56,7 @@ export default function Dashboard() {
       <h2>Projects and Working Groups</h2>
       <h2>Resources</h2>
       <h2>FAQs</h2>
-      <List>
-        <ListItem>
-          <ListItemText primary="Title" onClick={handleClick} />
-        </ListItem>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List disablePadding>
-            <ListItem>
-              <ListItemText primary="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." />
-            </ListItem>
-          </List>
-        </Collapse>
-      </List>
+      <List>{renderFAQs}</List>
     </div>
   );
 }
