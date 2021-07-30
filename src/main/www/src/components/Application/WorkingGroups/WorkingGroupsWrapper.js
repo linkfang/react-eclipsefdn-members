ef02ada50963f5167aab55a49bfdacee437df65c
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-07-30 11:26:47 -0400

    Added a modal window to confirm uncheck joining a wg

--- src/main/www/src/components/Application/WorkingGroups/WorkingGroupsWrapper.js
+++ src/main/www/src/components/Application/WorkingGroups/WorkingGroupsWrapper.js
@@ -20,7 +20,16 @@ import {
 } from '../../../Constants/Constants';
 import CustomStepButton from '../../UIComponents/Button/CustomStepButton';
 import { FormikProvider } from 'formik';
-import { Checkbox, FormControlLabel } from '@material-ui/core';
+import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControlLabel,
+} from '@material-ui/core';
 import { initialValues } from '../../UIComponents/FormComponents/formFieldModel';
 
 /**
@@ -47,26 +56,36 @@ const WorkingGroupsWrapper = ({ formik, isStartNewForm }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [workingGroupsUserJoined, setWorkingGroupsUserJoined] = useState([]);
   const [fullWorkingGroupList, setFullWorkingGroupList] = useState([]);
+  const [shouldOpen, setShouldOpen] = useState(false);
 
   const handleIsJoiningWG = () => {
     const isJoiningWG = formik.values.isJoiningWG;
 
     if (isJoiningWG) {
-      // if user uncheck it, then we need to reset WG form
-      formik.values.workingGroups.map((item) => {
-        deleteData(
-          currentFormId,
-          END_POINT.working_groups,
-          item.id,
-          formik.resetForm,
-          ''
-        );
-        return null;
-      });
+      setShouldOpen(true);
     } else {
       formik.setFieldValue('isJoiningWG', !isJoiningWG);
       formik.setFieldValue('workingGroups', initialValues.workingGroups);
     }
+  };
+
+  const closeModal = () => {
+    setShouldOpen(false);
+  };
+
+  const handleClearData = () => {
+    // if user uncheck it, then we need to reset WG form
+    formik.values.workingGroups.map((item) => {
+      deleteData(
+        currentFormId,
+        END_POINT.working_groups,
+        item.id,
+        formik.resetForm,
+        ''
+      );
+      return null;
+    });
+    closeModal();
   };
 
   useEffect(() => {
@@ -190,6 +209,29 @@ const WorkingGroupsWrapper = ({ formik, isStartNewForm }) => {
           id="working-groups-page"
           className="align-center margin-top-50 margin-bottom-30"
         >
+          <Dialog
+            open={shouldOpen}
+            onClose={closeModal}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {'Uncheck Joining a Working Group'}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                This will clear all saved data in this step. Proceed to
+                uncheck?
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={closeModal}>Cancel</Button>
+              <Button onClick={handleClearData} color="primary" autoFocus>
+                Yes
+              </Button>
+            </DialogActions>
+          </Dialog>
+
           <h1 className="fw-600 h2">Working Group</h1>
           <FormControlLabel
             control={
