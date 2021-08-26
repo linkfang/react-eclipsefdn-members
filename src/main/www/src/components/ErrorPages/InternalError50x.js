8149e9fdfca9aaf81d1cc06a5ae58cb36aa23515
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-08-26 16:22:42 -0400

    Added a return to home button for 50x page

--- src/main/www/src/components/ErrorPages/InternalError50x.js
+++ src/main/www/src/components/ErrorPages/InternalError50x.js
@@ -1,9 +1,14 @@
+import { Button } from "@material-ui/core";
+
 export default function InternalError50x() {
   return (
-    <>
+    <div className="margin-bottom-20">
       <h1>An unexpected error occurred</h1>
       <p>Sorry, the page you are looking for is currently unavailable.</p>
       <p>Please try again later.</p>
-    </>
+      <Button variant="contained" color="primary" onClick={() => window.location.assign(window.location.origin)}>
+        Return to Homepage
+      </Button>
+    </div>
   );
 }
