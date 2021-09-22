9b81468d62d640a86cf5a590b9be8ea2022d4286
Zhou Fang <zhou.fang@eclipse-foundation.org>
2021-09-22 14:47:50 -0400

    Added a check for callbackFunc

--- src/main/www/src/Utils/formFunctionHelpers.js
+++ src/main/www/src/Utils/formFunctionHelpers.js
@@ -481,7 +481,7 @@ function callSendData(formId, endpoint = '', dataBody, stepNum, setFieldValueObj
       .then((res) => {
         if (res.ok) {
           if (stepNum === 5) {
-            callbackFunc();
+            callbackFunc && callbackFunc();
             return res;
           }
           return res.json();
